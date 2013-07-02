e3789b723596cdebb731244d103582a2eb055c59
akunspy <akun.spy@gmail.com>
2013-07-02 18:20:46 +0800

    add example

---
+++ example/make.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+if [ ! -f make.sh ]; then
+	echo 'make.sh must be run within its container folder' 1>&2
+	exit 1
+fi
+
+
+CURDIR=`pwd`
+OLDGOPATH="$GOPATH"
+export GOPATH="$CURDIR":"$OLDGOPATH"
+go install main
+export GOPATH="$OLDGOPATH"
+
+if [ "$1" = "fmt" ]; then
+    gofmt -w src
+fi
