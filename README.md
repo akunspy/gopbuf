e5afd0e1bfad9af0471672599f15542ee58b75be
akunspy <akun.spy@gmail.com>
2013-07-03 13:44:21 +0800

    add benchmark

--- README.md
+++ README.md
@@ -1,12 +1,10 @@
-gopbuf
-======
-
 a protobuf compiler for golang
 
+## Requirements
+* Go 1.1 or higher        http://golang.org/
+* Protobuf 2.4 or higher  http://code.google.com/p/protobuf/
+
 ## Build
-
-install protobuf first, from http://code.google.com/p/protobuf/downloads/list
-
 ```
 cd src
 make
@@ -14,10 +12,36 @@ make install
 ```
 
 ## Test
-
 run these in shell
 ```
 cd example
 make
 bin/main
 ```
+
+## Benchmark
+
+<table>
+    <tr>
+        <th>Benchmark</th>
+        <th>gopbuf</th>
+        <th>goprotobuf</th>
+        <th>protobuf(cpp)</th>
+        <th>protobuf(cpp +O3)</th>
+    </tr>
+    <tr>
+        <th>Serialize</th>
+        <td>0.483s</td>
+        <td>1.364s</td>
+        <td>0.862s</td>
+        <td>0.141s</td>
+    </tr>
+    <tr>
+        <th>Parse</th>
+        <td>1.393s</td>
+        <td>2.165s</td>
+        <td>0.696s</td>
+        <td>0.231s</td>
+    </tr> 
+    
+</table>
--- example/src/proto/proto_test.go
+++ example/src/proto/proto_test.go
@@ -4,56 +4,56 @@ import "testing"
 
 func Test_Message(t *testing.T) {
 	sub := NewSub()
-    if sub.f1 != 8 || sub.f2 != 0 || sub.f3 != 0 {
-		t.Error( "Sub: test field default value error" )
-    }
-
-	if sub.Hasf1() || sub.Hasf2() || sub.HasF3() {
-		t.Error( "Sub: test Has error" )
+	if sub.f1 != 8 || sub.f2 != 0 || sub.f3 != 0 {
+		t.Error("Sub: test field default value error")
 	}
 
-	sub.Setf2( 5 )
+	if sub.Hasf1() || sub.Hasf2() || sub.HasF3() {
+		t.Error("Sub: test Has error")
+	}
+
+	sub.Setf2(5)
 	if !sub.Hasf2() || sub.Getf2() != 5 {
-		t.Error( "Sub: test Set/Get error" )
+		t.Error("Sub: test Set/Get error")
 	}
 
 	if sub.IsInitialized() {
-		t.Error( "Sub: test IsInitialized error" )
+		t.Error("Sub: test IsInitialized error")
 	}
-	sub.Setf1( 1 )
+	sub.Setf1(1)
 	if !sub.IsInitialized() {
-		t.Error( "Sub: test IsInitialized error" )
+		t.Error("Sub: test IsInitialized error")
 	}
 
-	buf := NewProtoBuffer( 100 )
-	err := sub.Serialize( buf )
+	buf := NewProtoBuffer(100)
+	err := sub.Serialize(buf)
 	if err != nil {
-		t.Error( "Sub: test Serialize error" )
+		t.Error("Sub: test Serialize error")
 	}
 
 	newsub := NewSub()
-	err = newsub.Parse( buf, sub.ByteSize() )
+	err = newsub.Parse(buf, sub.ByteSize())
 	if err != nil {
-		t.Error( "Sub: test Parse error" )
+		t.Error("Sub: test Parse error")
 	}
 
 	if sub.Getf1() != newsub.Getf1() ||
 		sub.Getf2() != newsub.Getf2() ||
 		sub.GetF3() != newsub.GetF3() {
-		t.Error( "Sub: test Parse error" )
+		t.Error("Sub: test Parse error")
 	}
 	if !newsub.Hasf1() || !newsub.Hasf2() || newsub.HasF3() {
-		t.Error( "Sub: test Parse error" )
+		t.Error("Sub: test Parse error")
 	}
 
 	sub.Clear()
 	if sub.IsInitialized() {
-		t.Error( "Sub: test Clear error" )
+		t.Error("Sub: test Clear error")
 	}
-    if sub.f1 != 8 || sub.f2 != 0 || sub.f3 != 0 {
-		t.Error( "Sub: test Clear error" )
-    }
+	if sub.f1 != 8 || sub.f2 != 0 || sub.f3 != 0 {
+		t.Error("Sub: test Clear error")
+	}
 	if sub.Hasf1() || sub.Hasf2() || sub.HasF3() {
-		t.Error( "Sub: test Clear error" )
+		t.Error("Sub: test Clear error")
 	}
 }
